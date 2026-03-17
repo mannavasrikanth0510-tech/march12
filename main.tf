b50583ec288acc52b4c4e62b73f028669dcc62b0
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-17 08:32:23 -0500

    updatedmain

--- main.tf
+++ main.tf
@@ -9,29 +9,3 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = var.environment
   }
 }
-
-resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-  bucket = aws_s3_bucket.my_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "my_bucket_sse" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "my_bucket_pab" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
