aa4e4cdf82304d2f80e64f11693b847514e8d9a7
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-17 10:46:20 -0500

    personal brach commits (#1)
    * personal brach commits
    
    * Update main.tf
    
    * Update output.tf
    
    * force ci rerun
    
    * Update main.tf
    
    * Update output.tf
    
    * Update output.tf
    
    * Update variable.tf
    
    * Update README.md
    
    * added some settings
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update main.tf
    
    * updatedmain
    
    * Force Update README.md
    
    * Update sample
    
    * Update README.md
    
    * Update README.md updated
    
    * Update README.md new
    
    * new main
    
    * new main changes
    
    * Update README.md

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_id" {
+  value       = aws_s3_bucket.my_bucket.id
+  description = "The name of the S3 bucket"
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.my_bucket.arn
+  description = "The ARN of the S3 bucket"
+}
