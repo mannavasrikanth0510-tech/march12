a942bef6a7bb9ca4b8ab068bfcf28dbf28811d32
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-20 10:02:38 -0500

    new vpc (#6)
    * vpc
    
    * ci/cd
    
    * new-vpc
    
    * v
    
    * v
    
    * Update backend.tf
    
    * Create sample
    
    * Update sample
    
    * Rename backend.tf to backend.hcl
    
    * Update backend.hcl
    
    * Update sample
    
    * Update sample
    
    * Create storage
    
    * Delete infra/storage
    
    * Create storage
    
    * Delete infra/storage
    
    * 19 changes
    
    * Update sample
    
    * Update sample
    
    * new file 19
    
    * Update vpc-main.tf
    
    * Update vpc-main.tf
    
    * logs
    
    * Update sample
    
    * Update flow-logs.tf
    
    * Update vpc-main.tf
    
    * Update flow-logs.tf
    
    * Restrict log stream resource to specific stream
    
    * Update file indicator in sample
    
    * Rename file from 'file..' to 'file...'
    
    * Rename file in network directory
    
    * Update file name to include a period
    
    * Add S3 backend configuration for Terraform
    
    Configure S3 backend for Terraform state management.
    
    * Update S3 backend configuration in providers.tf
    
    * Add DynamoDB table for state locking
    
    * Update DynamoDB table name for Terraform state
    
    * Update Terraform required version and formatting
    
    * Fix formatting in providers.tf
    
    * Remove trailing newline in providers.tf
    
    * Refactor S3 backend configuration
    
    Simplified S3 backend configuration in providers.tf.
    
    * Add DynamoDB table resource for terraform locks
    
    * Delete infra/network/dynamo.tf
    
    * Add bucket_name variable to VPC configuration
    
    * Add default value for aws_region variable
    
    * Fix formatting of variable type declarations
    
    * Update vpc-variables.tf
    
    * Fix formatting issue in vpc-variables.tf
    
    * Fix missing newline at end of vpc-variables.tf
    
    * Update file indicator from 'file.' to 'file..'
    
    * Rename file from 'file..' to 'file...'
    
    * Refactor VPC variables and add subnet CIDRs
    
    Removed unused VPC variables and added CIDR variables for public subnets.

---
+++ infra/network/flow-logs.tf
@@ -0,0 +1,89 @@
+resource "aws_kms_key" "flow_logs" {
+  description             = "CMK for VPC Flow Logs (${var.environment})"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name        = "kms-flow-logs-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_kms_alias" "flow_logs" {
+  name          = "alias/vpc-flow-logs-${var.environment}"
+  target_key_id = aws_kms_key.flow_logs.key_id
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/flow-logs/${var.environment}"
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.flow_logs.arn
+
+  tags = {
+    Name        = "vpc-flow-logs-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role" "vpc_flow_logs_role" {
+  name = "vpc-flow-logs-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "vpc_flow_logs_policy_doc" {
+  statement {
+    sid    = "AllowCreateAndWriteFlowLogStreams"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:log-stream:vpc-flow-logs-stream"
+    ]
+  }
+
+  statement {
+    sid    = "AllowDescribeFlowLogGroup"
+    effect = "Allow"
+    actions = [
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.vpc_flow_logs.arn
+    ]
+  }
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+resource "aws_iam_role_policy" "vpc_flow_logs_policy" {
+  name   = "vpc-flow-logs-policy-${var.environment}"
+  role   = aws_iam_role.vpc_flow_logs_role.id
+  policy = data.aws_iam_policy_document.vpc_flow_logs_policy_doc.json
+}
+
+resource "aws_flow_log" "main" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_group_name       = aws_cloudwatch_log_group.vpc_flow_logs.name
+  iam_role_arn         = aws_iam_role.vpc_flow_logs_role.arn
+
+  tags = {
+    Name        = "vpc-flow-log-${var.environment}"
+    Environment = var.environment
+  }
+}
