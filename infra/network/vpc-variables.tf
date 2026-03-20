a8c578eb2cd1038bd76248b036d20a6b3ad507eb
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-20 09:44:44 -0500

    Update vpc-variables.tf

--- infra/network/vpc-variables.tf
+++ infra/network/vpc-variables.tf
@@ -3,7 +3,7 @@ variable "bucket_name" {
 }
 
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
@@ -12,26 +12,26 @@ variable "environment" {
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_1_cidr" {
-  type = string
+  type    = string
   default = "10.0.1.0/24"
 }
 
 variable "public_subnet_2_cidr" {
-  type = string
+  type    = string
   default = "10.0.2.0/24"
 }
 
 variable "private_subnet_1_cidr" {
-  type = string
+  type    = string
   default = "10.0.11.0/24"
 }
 
 variable "private_subnet_2_cidr" {
-  type = string
+  type    = string
   default = "10.0.12.0/24"
 }
