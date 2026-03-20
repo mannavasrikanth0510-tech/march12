e3df364ee3833c47ae36444219815622bf906ce6
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-20 09:47:12 -0500

    Formatted and updated VPC variables in terraform code with aligned spacing

--- infra/network/vpc-variables.tf
+++ infra/network/vpc-variables.tf
@@ -1,32 +1,35 @@
-variable "aws_region" {
-  type = string
-}
+variable "vpc_id" {  
+  description = "The ID of the VPC"  
+  type        = string  
+  default     = null  
+}  
 
-variable "environment" {
-  type = string
-}
+variable "region" {  
+  description = "The AWS region where the VPC is located"  
+  type        = string  
+  default     = "us-east-1"  
+}  
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
+variable "availability_zones" {  
+  description = "A list of availability zones to use for the VPC"  
+  type        = list(string)  
+  default     = ["us-east-1a", "us-east-1b"]  
+}  
 
-variable "public_subnet_1_cidr" {
-  type    = string
-  default = "10.0.1.0/24"
-}
+variable "cidr_block" {  
+  description = "The CIDR block for the VPC"  
+  type        = string  
+  default     = "10.0.0.0/16"  
+}  
 
-variable "public_subnet_2_cidr" {
-  type    = string
-  default = "10.0.2.0/24"
-}
+variable "enable_dns_support" {  
+  description = "Whether DNS resolution is supported"  
+  type        = bool  
+  default     = true  
+}  
 
-variable "private_subnet_1_cidr" {
-  type    = string
-  default = "10.0.11.0/24"
-}
-
-variable "private_subnet_2_cidr" {
-  type    = string
-  default = "10.0.12.0/24"
-}
+variable "enable_dns_hostnames" {  
+  description = "Whether DNS hostnames are enabled"  
+  type        = bool  
+  default     = true  
+}  
