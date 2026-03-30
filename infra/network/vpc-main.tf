f918b0a0cc89550e11fe9f9b48f7599a808f184a
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-30 15:00:02 -0500

    v

--- infra/network/vpc-main.tf
+++ infra/network/vpc-main.tf
@@ -123,24 +123,14 @@ resource "aws_route_table_association" "private_2_assoc" {
 ##############################
 resource "aws_security_group" "alb_sg" {
   name        = "alb-sg-${var.environment}"
-  description = "ALB SG: public internet HTTP/HTTPS"
-
-resource "aws_security_group" "app_sg" {
-  name_prefix = "app-sg-${var.environment}-"
-
-  description = "EC2 SG allow ALB > EC2 only"
+  description = "ALB SG public internet HTTP HTTPS"
   vpc_id      = aws_vpc.main.id
-  lifecycle {
-    create_before_destroy = true
-  }
 
   ingress {
     description = "HTTP redirect to HTTPS"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-
-    #tfsec:ignore:aws-ec2-no-public-ingress-sgr
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -149,8 +139,6 @@ resource "aws_security_group" "app_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-
-    #tfsec:ignore:aws-ec2-no-public-ingress-sgr
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -159,8 +147,6 @@ resource "aws_security_group" "app_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-
-    #tfsec:ignore:aws-ec2-no-public-egress-sgr
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -168,6 +154,35 @@ resource "aws_security_group" "app_sg" {
     Name = "alb-sg-${var.environment}"
   }
 }
+
+resource "aws_security_group" "app_sg" {
+  name_prefix = "app-sg-${var.environment}-"
+  description = "EC2 SG allow ALB > EC2 only"
+  vpc_id      = aws_vpc.main.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ingress {
+    description     = "App from ALB"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg-${var.environment}"
+  }
 }
 
 ##############################
