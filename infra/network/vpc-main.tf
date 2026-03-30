c1b5c0cd6058a919974d6014ad1b39598a09a6e1
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-30 15:10:09 -0500

    q

--- infra/network/vpc-main.tf
+++ infra/network/vpc-main.tf
@@ -192,39 +192,7 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-##############################
-# Security Group - EC2 (private subnet)
-########
-#tfsec:ignore:aws-ec2-no-public-egress-sgr
-resource "aws_security_group" "app_sg" {
-  name_prefix = "app-sg-${var.environment}-"
-  description = "EC2 SG allow ALB > EC2 only"
-  vpc_id      = aws_vpc.main.id
 
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  ingress {
-    description     = "App from ALB"
-    from_port       = var.app_port
-    to_port         = var.app_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    description = "Allow all outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "app-sg-${var.environment}"
-  }
-}
 
 ##############################
 # Public ALB
