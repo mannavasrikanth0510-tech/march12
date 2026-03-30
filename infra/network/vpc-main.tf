59023965ac73e71f0b62b5ecd9669b475dc35ebb
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-03-30 11:46:30 -0500

    c

--- infra/network/vpc-main.tf
+++ infra/network/vpc-main.tf
@@ -166,7 +166,7 @@ resource "aws_security_group" "alb_sg" {
 #tfsec:ignore:aws-ec2-no-public-egress-sgr
 resource "aws_security_group" "app_sg" {
   name        = "app-sg-${var.environment}"
-  description = "EC2 SG: allow ALB to EC2 only"
+  description = "EC2 SG: allow ALB > EC2 only"
   vpc_id      = aws_vpc.main.id
 
   ingress {
