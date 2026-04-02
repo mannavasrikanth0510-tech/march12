7dfd42d33a36ab36bce667b93cd7caf3395d629a
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-02 15:27:56 -0500

    closed

--- infra/network/vpc-main.tf
+++ infra/network/vpc-main.tf
@@ -377,7 +377,6 @@ EOF
   tags = {
     Name = "app-${var.environment}"
   }             
-}
 
 resource "aws_lb_target_group_attachment" "app_attach" {
   target_group_arn = aws_lb_target_group.app_tg.arn
