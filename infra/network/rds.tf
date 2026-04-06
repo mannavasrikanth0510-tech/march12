463b3ed970f436303828894c1a73bcf55521bfdb
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-06 09:46:03 -0500

    rds changes

--- infra/network/rds.tf
+++ infra/network/rds.tf
@@ -46,5 +46,4 @@ resource "aws_db_instance" "app_db" {
   db_subnet_group_name                = aws_db_subnet_group.main.name
   vpc_security_group_ids              = [aws_security_group.db_sg.id]
   multi_az                            = false
-  performance_insights_enabled        = true # (Optional, but recommended)
 }
